fe942ae095a96cd63e97cd6b43d43e158227ae07
Petr Košťál <kostape4@fit.cvut.cz>
2025-07-18 07:14:01 +0200

    Add loss and value framework templates.

---
+++ dl_tools/loss.py
@@ -0,0 +1,69 @@
+import torch
+
+"""
+Implementation of losses for determinstic predictions.
+All are inherited from general `torch.nn.Module`.
+"""
+
+"""
+    #TODO
+    - implement ASYMmloss
+    - test all implementations
+"""
+
+class NNLGammaLoss(torch.nn.Module):
+    def __init__(self) -> None:
+        super(NNLGammaLoss, self).__init__()
+    
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """
+        input:  [batch_size, shape || scale]
+        target: [batch_size, grid_size]
+        """
+
+        grid_size = target.shape[0]
+        prob = input[:, grid_size:]
+        shape = input[:, grid_size:(2 * grid_size)]
+        scale = input[:, (2 * grid_size):]
+
+        loss = \
+            (shape * torch.log(scale) + torch.lgamma(shape) + target / scale) / \
+            (torch.log(prob) + (shape - 1) * torch.log(target))
+        
+        return -torch.mean(loss)
+
+class NNLBernoulliGammaLoss(torch.nn.Module):
+    def __init__(self) -> None:
+        super(NNLBernoulliGammaLoss, self).__init__()
+    
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """
+        input:  [batch_size, probability || shape || scale]
+        target: [batch_size, grid_size]
+        """
+        
+        grid_size = target.shape[0]
+        prob = input[:, grid_size:]
+        shape = input[:, grid_size:(2 * grid_size)]
+        scale = input[:, (2 * grid_size):]
+
+        zero_mask = [input == 0].type(torch.float32)
+        zero_case = zero_mask * torch.log(1 - prob)
+
+        nonzero_case = ~zero_mask * \
+            (shape * torch.log(scale) + torch.lgamma(shape) + target / scale) / \
+            (torch.log(prob) + (shape - 1) * torch.log(target))
+        
+        return -torch.mean(zero_case + nonzero_case)
+
+"""
+ASYM Calculate distribution beforehand.
+"""
+
+class ASYMmetricLoss(torch.nn.Module):
+    def __init__(self) -> None:
+        super(ASYMmetricLoss, self).__init__()
+        raise NotImplementedError()
+    
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError()
---
+++ dl_tools/value.py
@@ -0,0 +1,86 @@
+import torch 
+from typing import Set, List
+
+"""
+    #TODO
+    - implement methods and validations for evaluation
+    - store to tensorboard
+"""
+
+
+class VALUE:
+
+    """Supported variables."""
+    VARIABLES: Set[str] = {"PR", "TM", "TN", "TX"}
+
+    class Variable:
+        def __init__(
+            self,
+            prediction: List[torch.Tensor],
+            variable: List[str],
+            stochastic: bool = False    
+        ) -> None:
+            raise NotImplementedError()
+
+        
+    def __init__(self) -> None:
+        self.stochastic_predictions: List[VALUE.Variable] = []
+        self.deterministic_predictions: List[VALUE.Variable] = []
+
+
+    def add_prediction(self, prediction: List[torch.Tensor]) -> None:
+        raise NotImplementedError()
+
+    def evaluate(self) -> None:
+        raise NotImplementedError()
+    
+    def _lag_1_autocorrelation(self) -> float:
+        raise NotImplementedError()
+
+    def _median_of_the_annual_cold(self) -> float:
+        raise NotImplementedError()
+
+    def _median_of_the_annual_dry(self) -> float:
+        raise NotImplementedError()
+
+    def _relative_frequency_of_days_plus_20_deg(self) -> float:
+        raise NotImplementedError()
+
+    def _relative_frequency_of_days_plus_25_deg(self) -> float:
+        raise NotImplementedError()
+
+    def _relative_frequency_of_days_minus_0_deg(self) -> float:
+        raise NotImplementedError()
+
+    def _mean(self) -> float:
+        raise NotImplementedError()
+
+    def _2nd_percentile(self) -> float:
+        raise NotImplementedError()
+
+    def _98th_percentile(self) -> float:
+        raise NotImplementedError()
+
+    def _mean_wet_day_precipitation(self) -> float:
+        raise NotImplementedError()
+
+    def _median_of_the_annual_warm(self) -> float:
+        raise NotImplementedError()
+
+    def _median_of_the_annual_wet(self) -> float:
+        raise NotImplementedError()
+
+    def _kolmogorov_smirnov(self) -> float:
+        raise NotImplementedError()
+
+    def _pearson_correlation(self) -> float:
+        raise NotImplementedError()
+
+    def _ration_of_the_standard_deviations(self) -> float:
+        raise NotImplementedError()
+
+    def _root_mean_squre_error(self) -> float:
+        raise NotImplementedError()
+
+    def spearman_correlation(self) -> float:
+        raise NotImplementedError()
