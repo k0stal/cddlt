910a263605dcd665ecbc04bb78d5fc604b96fb9b
Petr Košťál <kostape4@fit.cvut.cz>
2025-08-05 18:35:14 +0200

    Add init hyperparams

--- cddlt/models/edsr.py
+++ cddlt/models/edsr.py
@@ -3,7 +3,10 @@ import torch.nn
 import cddlt
 
 class ResidualBlock(torch.nn.Module):
-    def __init__(self, features):
+    def __init__(
+        self, 
+        features: int
+    ) -> None:
         super().__init__()
         self.residual_block = torch.nn.Sequential(
             torch.nn.Conv2d(features, features, kernel_size=3, padding=1),
@@ -11,7 +14,10 @@ class ResidualBlock(torch.nn.Module):
             torch.nn.Conv2d(features, features, kernel_size=3, padding=1),
         )
 
-    def forward(self, x):
+    def forward(
+        self, 
+        x: torch.Tensor
+    ) -> torch.Tensor:
         residue = self.residual_block(x)
         # residual scaling
         # TODO add reference
@@ -21,7 +27,11 @@ class ResidualBlock(torch.nn.Module):
 
 
 class Upsampler(torch.nn.Sequential):
-    def __init__(self, scale_factor, features):
+    def __init__(
+        self, 
+        scale_factor: int, 
+        features: int
+    ) -> None:
         # TODO ascending or descending order?
         prime_factors = sympy.factorint(scale_factor, multiple=True)
         modules = []
@@ -38,7 +48,13 @@ class Upsampler(torch.nn.Sequential):
 class EDSR(cddlt.DLModule):
     # official implementation:
     # https://github.com/sanghyun-son/EDSR-PyTorch/tree/master
-    def __init__(self, channels, scale_factor):
+    def __init__(
+        self, 
+        channels: int, 
+        scale_factor: int,
+        features: int = 256,
+        residual_blocks: int = 32,
+    ) -> None:
         super().__init__()
         # Lim et al. (2017):
         # "We train our networks using L1 loss instead of L2. Minimizing L2 is
@@ -47,21 +63,24 @@ class EDSR(cddlt.DLModule):
         # better convergence than L2. The evaluation of this comparison is
         # provided in Sec. 4.4"
         self.loss = torch.nn.L1Loss()
-        features = 256
-        residual_blocks = 32
+        self.features = features
+        self.residual_blocks = residual_blocks
         self.head = torch.nn.Sequential(
-            torch.nn.Conv2d(channels, features, kernel_size=3, padding=1)
+            torch.nn.Conv2d(channels, self.features, kernel_size=3, padding=1)
         )
         self.body = torch.nn.Sequential(
-            *[ResidualBlock(features) for _ in range(residual_blocks)],
-            torch.nn.Conv2d(features, features, kernel_size=3, padding=1),
+            *[ResidualBlock(self.features) for _ in range(self.residual_blocks)],
+            torch.nn.Conv2d(self.features, self.features, kernel_size=3, padding=1),
         )
         self.tail = torch.nn.Sequential(
-            Upsampler(scale_factor, features),
-            torch.nn.Conv2d(features, channels, kernel_size=3, padding=1),
+            Upsampler(scale_factor, self.features),
+            torch.nn.Conv2d(self.features, channels, kernel_size=3, padding=1),
         )
 
-    def forward(self, x):
+    def forward(
+        self, 
+        x: torch.Tensor
+    ) -> torch.Tensor:
         x = self.head(x)
         residue = self.body(x)
         residue += x
