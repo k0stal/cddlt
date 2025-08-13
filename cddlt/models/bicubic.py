aaa0ac9df5ef275b467cc6d7a88001318dddb1df
Petr Košťál <kostape4@fit.cvut.cz>
2025-08-13 19:58:10 +0200

    Add mode parameter

--- cddlt/models/bicubic.py
+++ cddlt/models/bicubic.py
@@ -2,15 +2,16 @@ import torch
 import cddlt
 
 class Bicubic(cddlt.DLModule):
-    def __init__(self, upscale_factor: int) -> None:
+    def __init__(self, upscale_factor: int, mode: str = "bicubic") -> None:
         super().__init__()
         self.upscale_factor = upscale_factor
+        self.mode = mode
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = torch.nn.functional.interpolate(
             x,
             scale_factor=self.upscale_factor,
-            mode='bicubic',
+            mode=self.mode,
             align_corners=False,
             antialias=True
         )
